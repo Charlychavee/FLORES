ee3e5eb6d204bb36dadb80d3c8989a951ad1a50a
Jose Carlos Felix Chavez <charlychavee2@gmail.com>
2024-09-21 14:03:06 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
+function iniciar(){
+
+        const body = document.body;
+
+        body.classList.remove('body');
+        body.classList.add('body2');
+
+        let divInicio = document.getElementById("divInicio");
+        let divFlores = document.getElementById("divFlores");
+
+        divInicio.style.display = "none";
+        divFlores.style.display = "block";
+
+        let audio = document.getElementById("player");
+        audio.play();
+
+        document.body.classList.remove("container");    
+}
